79739ef89ed773b61c65bf0217a2a5fe13fbe141
kysalol <60238122+kysalol@users.noreply.github.com>
2021-02-11 22:23:06 -0600

    Update practica1.ino

--- practica1.ino
+++ practica1.ino
@@ -8,68 +8,148 @@ void setup()
   pantalla.begin(13, 11, 12, 2);
   pantalla.rotar(true);
 
+
 }
 
   
 void loop() { 
 
+    pinMode(3, INPUT);
+    pinMode(1,INPUT);
+     
+    if (digitalRead(3) == HIGH) {
+          movizquierda();           
+    }else{         
+          movderecha();    
+    }
+}
 
- 
-  
+
+
+
+void movderecha()
+{
+
+
+
+ if (digitalRead(3) != HIGH) {
+
  //  Desplazar texto manualmente hacia la derecha
   pantalla.borrar();
-  for(int i=-80; i<30; i++){
+  for(int i=-80; i<30; i++){  
     pantalla.escribirFrase("SECCION A*", i); // Texto, posicion en la pantalla
+    
+    if (digitalRead(1) == HIGH) {
     delay(10);
+    }else {
+      delay(500);
+      }
   }
+ }
 
 
+
+ if (digitalRead(3) != HIGH) {
+ 
   guionder();
+
+ }
   
   
+ if (digitalRead(3) != HIGH) {
   pantalla.borrar();
   for(int i=-65; i<30; i++){
     pantalla.escribirFrase("GRUPO17", i); // Texto, posicion en la pantalla
+    if (digitalRead(1) == HIGH) {
     delay(10);
+    }else {
+      delay(500);
+      }
   }
+ }
 
+
+ if (digitalRead(3) != HIGH) {
 
   guionder();
+ }
 
 
+
+ if (digitalRead(3) != HIGH) {
     pantalla.borrar();
   for(int i=-30; i<30; i++){
     pantalla.escribirFrase("*TP1", i); // Texto, posicion en la pantalla
+    if (digitalRead(1) == HIGH) {
     delay(10);
+    }else {
+      delay(500);
+      }
   }
+ }
+}
 
 
 
+void movizquierda()
+{
 
-    // Desplazar texto manualmente hacia la IZQUIERDA
+  
+ if (digitalRead(3) == HIGH) {
+      // Desplazar texto manualmente hacia la IZQUIERDA
   pantalla.borrar();
   for(int i = 30; i > -50; i--){
     pantalla.escribirFrase("*TP1", i); // Texto, posicion en la pantalla
+    if (digitalRead(1) == HIGH) {
     delay(10);
+    }else {
+      delay(500);
+      }
   }
+ }
 
+
+
+
+ if (digitalRead(3) == HIGH) {
   pantalla.borrar();
   guioniz(); 
+
+ }
+
+if (digitalRead(3) == HIGH) {
 
   pantalla.borrar();
   for(int i = 30; i > -60; i--){
     pantalla.escribirFrase("GRUPO17", i); // Texto, posicion en la pantalla
+    if (digitalRead(1) == HIGH) {
     delay(10);
+    }else {
+      delay(500);
+      }
   }
+}
 
+
+if (digitalRead(3) == HIGH) {
   pantalla.borrar();
   guioniz();
 
+
+}
+
+
+if (digitalRead(3) == HIGH) {
     pantalla.borrar();
   for(int i = 30; i > -75; i--){
     pantalla.escribirFrase("SECCION A*", i); // Texto, posicion en la pantalla
+    if (digitalRead(1) == HIGH) {
     delay(10);
+    }else {
+      delay(500);
+      }
   }
+}
 
 
 }
@@ -78,83 +158,178 @@ void guioniz()
 {
     pantalla.borrar();
       pantalla.setRow(0, 3, B10000000);
-      delay(10);
+    if (digitalRead(1) == HIGH) {
+    delay(10);
+    }else {
+      delay(500);
+      }
       pantalla.borrar();
       pantalla.setRow(0, 3, B11000000);
-      delay(10);
+    if (digitalRead(1) == HIGH) {
+    delay(10);
+    }else {
+      delay(500);
+      }
       pantalla.borrar();
       pantalla.setRow(0, 3, B11100000);
+    if (digitalRead(1) == HIGH) {
+    delay(10);
+    }else {
       delay(500);
+      }
       pantalla.borrar();
       pantalla.setRow(0, 3, B11110000);
-      delay(10);
+    if (digitalRead(1) == HIGH) {
+    delay(10);
+    }else {
+      delay(500);
+      }
       pantalla.borrar();
       pantalla.setRow(0, 3, B11111000);
-      delay(10);
+    if (digitalRead(1) == HIGH) {
+    delay(10);
+    }else {
+      delay(500);
+      }
       pantalla.borrar();
       pantalla.setRow(0, 3, B11111100);
-      delay(10);
+    if (digitalRead(1) == HIGH) {
+    delay(10);
+    }else {
+      delay(500);
+      }
       pantalla.borrar();
       pantalla.setRow(0, 3, B11111110);
+      delay(10);    if (digitalRead(1) == HIGH) {
+    delay(10);
+    }else {
       delay(500);
-      pantalla.borrar();
+      }      pantalla.borrar();
       pantalla.setRow(0, 3, B11111111);
-      delay(10);
+    if (digitalRead(1) == HIGH) {
+    delay(10);
+    }else {
+      delay(500);
+      }
       pantalla.borrar();
       pantalla.setRow(1, 3, B10000000);
       pantalla.setRow(0, 3, B01111111);
-      delay(10);
+    if (digitalRead(1) == HIGH) {
+    delay(10);
+    }else {
+      delay(500);
+      }
       pantalla.borrar();
       pantalla.setRow(1, 3, B11000000);
       pantalla.setRow(0, 3, B00111111);
-      delay(10);
+    if (digitalRead(1) == HIGH) {
+    delay(10);
+    }else {
+      delay(500);
+      }
       pantalla.borrar();
       pantalla.setRow(1, 3, B11100000);
       pantalla.setRow(0, 3, B00011111);
-      delay(10);
+    if (digitalRead(1) == HIGH) {
+    delay(10);
+    }else {
+      delay(500);
+      }
       pantalla.borrar();
       pantalla.setRow(1, 3, B11110000);
       pantalla.setRow(0, 3, B00001111);
-      delay(10);
+      if (digitalRead(1) == HIGH) {
+    delay(10);
+    }else {
+      delay(500);
+      }
       pantalla.borrar();      
       pantalla.setRow(1, 3, B11111000);
       pantalla.setRow(0, 3, B00000111);
-      delay(10);
+      if (digitalRead(1) == HIGH) {
+    delay(10);
+    }else {
+      delay(500);
+      }
       pantalla.borrar();
       pantalla.setRow(1, 3, B11111100);
       pantalla.setRow(0, 3, B00000011);
-      delay(10);
+      if (digitalRead(1) == HIGH) {
+    delay(10);
+    }else {
+      delay(500);
+      }
       pantalla.borrar();
       pantalla.setRow(1, 3, B11111110);
       pantalla.setRow(0, 3, B00000001);
+     if (digitalRead(1) == HIGH) {
+    delay(10);
+    }else {
       delay(500);
+      }
       pantalla.borrar();
       pantalla.setRow(1, 3, B11111111);
       pantalla.setRow(0, 3, B00000000);
+      if (digitalRead(1) == HIGH) {
+    delay(10);
+    }else {
       delay(500);
+      }
       pantalla.setRow(1, 3, B01111111);
-      delay(10);
+    if (digitalRead(1) == HIGH) {
+    delay(10);
+    }else {
+      delay(500);
+      }
       pantalla.borrar();
       pantalla.setRow(1, 3, B00111111);
-      delay(10);
+    if (digitalRead(1) == HIGH) {
+    delay(10);
+    }else {
+      delay(500);
+      }
       pantalla.borrar();
       pantalla.setRow(1, 3, B00011111);
-      delay(10);
+    if (digitalRead(1) == HIGH) {
+    delay(10);
+    }else {
+      delay(500);
+      }
       pantalla.borrar();
       pantalla.setRow(1, 3, B00001111);
-      delay(10);
+    if (digitalRead(1) == HIGH) {
+    delay(10);
+    }else {
+      delay(500);
+      }
       pantalla.borrar();
       pantalla.setRow(1, 3, B00000111);
-      delay(10);
+    if (digitalRead(1) == HIGH) {
+    delay(10);
+    }else {
+      delay(500);
+      }
       pantalla.borrar();
       pantalla.setRow(1, 3, B00000011);
-      delay(10);
+    if (digitalRead(1) == HIGH) {
+    delay(10);
+    }else {
+      delay(500);
+      }
       pantalla.borrar();
       pantalla.setRow(1, 3, B00000001);
-      delay(10);
+    if (digitalRead(1) == HIGH) {
+    delay(10);
+    }else {
+      delay(500);
+      }
       pantalla.borrar();
       pantalla.setRow(1, 3, B00000000);
-      delay(10);
+      if (digitalRead(1) == HIGH) {
+    delay(10);
+    }else {
+      delay(500);
+      }
       pantalla.borrar();
       
 }
@@ -164,86 +339,179 @@ void guionder()
 {
     pantalla.borrar();
       pantalla.setRow(1, 3, B00000001);
+    if (digitalRead(1) == HIGH) {
+    delay(10);
+    }else {
       delay(500);
+      }
       pantalla.borrar();
       pantalla.setRow(1, 3, B00000011);
+    if (digitalRead(1) == HIGH) {
+    delay(10);
+    }else {
       delay(500);
+      }
       pantalla.borrar();
       pantalla.setRow(1, 3, B00000111);
+    if (digitalRead(1) == HIGH) {
+    delay(10);
+    }else {
       delay(500);
+      }
       pantalla.borrar();
       pantalla.setRow(1, 3, B00001111);
+    if (digitalRead(1) == HIGH) {
+    delay(10);
+    }else {
       delay(500);
+      }
       pantalla.borrar();
       pantalla.setRow(1, 3, B00011111);
+    if (digitalRead(1) == HIGH) {
+    delay(10);
+    }else {
       delay(500);
+      }
       pantalla.borrar();
       pantalla.setRow(1, 3, B00111111);
+    if (digitalRead(1) == HIGH) {
+    delay(10);
+    }else {
       delay(500);
+      }
       pantalla.borrar();
       pantalla.setRow(1, 3, B01111111);
+    if (digitalRead(1) == HIGH) {
+    delay(10);
+    }else {
       delay(500);
+      }
       pantalla.borrar();
       pantalla.setRow(1, 3, B11111111);
+    if (digitalRead(1) == HIGH) {
+    delay(10);
+    }else {
       delay(500);
+      }
       pantalla.borrar();
       pantalla.setRow(0, 3, B00000001);
       pantalla.setRow(1, 3, B11111110);
-      delay(500);   
+    if (digitalRead(1) == HIGH) {
+    delay(10);
+    }else {
+      delay(500);
+      } 
       pantalla.borrar();
       pantalla.setRow(0, 3, B00000011);
       pantalla.setRow(1, 3, B11111100);
+    if (digitalRead(1) == HIGH) {
+    delay(10);
+    }else {
       delay(500);
+      }
       pantalla.borrar();
       pantalla.setRow(0, 3, B00000111);
       pantalla.setRow(1, 3, B11111000);
+    if (digitalRead(1) == HIGH) {
+    delay(10);
+    }else {
       delay(500);
+      }
       pantalla.borrar();
       pantalla.setRow(0, 3, B00001111);
       pantalla.setRow(1, 3, B11110000);
+    if (digitalRead(1) == HIGH) {
+    delay(10);
+    }else {
       delay(500);
+      }
       pantalla.borrar();      
       pantalla.setRow(0, 3, B00011111);
       pantalla.setRow(1, 3, B11100000);
+    if (digitalRead(1) == HIGH) {
+    delay(10);
+    }else {
       delay(500);
+      }
       pantalla.borrar();
       pantalla.setRow(0, 3, B00111111);
       pantalla.setRow(1, 3, B11000000);
+    if (digitalRead(1) == HIGH) {
+    delay(10);
+    }else {
       delay(500);
+      }
       pantalla.borrar();
       pantalla.setRow(0, 3, B01111111);
       pantalla.setRow(1, 3, B10000000);
+    if (digitalRead(1) == HIGH) {
+    delay(10);
+    }else {
       delay(500);
+      }
       pantalla.borrar();
       pantalla.setRow(0, 3, B11111111);
       pantalla.setRow(1, 3, B00000000);
+    if (digitalRead(1) == HIGH) {
+    delay(10);
+    }else {
       delay(500);
-      
+      }  
       pantalla.setRow(0, 3, B11111110);
+    if (digitalRead(1) == HIGH) {
+    delay(10);
+    }else {
       delay(500);
+      }
       pantalla.borrar();
       pantalla.setRow(0, 3, B11111100);
+    if (digitalRead(1) == HIGH) {
+    delay(10);
+    }else {
       delay(500);
+      }
       pantalla.borrar();
       pantalla.setRow(0, 3, B11111000);
+    if (digitalRead(1) == HIGH) {
+    delay(10);
+    }else {
       delay(500);
+      }
       pantalla.borrar();
       pantalla.setRow(0, 3, B11110000);
+    if (digitalRead(1) == HIGH) {
+    delay(10);
+    }else {
       delay(500);
+      }
       pantalla.borrar();
       pantalla.setRow(0, 3, B11100000);
+    if (digitalRead(1) == HIGH) {
+    delay(10);
+    }else {
       delay(500);
+      }
       pantalla.borrar();
       pantalla.setRow(0, 3, B11000000);
+    if (digitalRead(1) == HIGH) {
+    delay(10);
+    }else {
       delay(500);
+      }
       pantalla.borrar();
       pantalla.setRow(0, 3, B10000000);
+    if (digitalRead(1) == HIGH) {
+    delay(10);
+    }else {
       delay(500);
+      }
       pantalla.borrar();
       pantalla.setRow(0, 3, B00000000);
+    if (digitalRead(1) == HIGH) {
+    delay(10);
+    }else {
       delay(500);
+      }
       pantalla.borrar();
       
 }
-
-
